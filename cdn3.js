00e9c7ea29cd8cb2b70dbbee62712f41c18dbfa8
nabil <mahmoud.nabil663@gmail.com>
2017-05-23 21:15:47 +0200

    'first'

---
+++ cdn3.js
@@ -0,0 +1,15 @@
+
+console.log('Testing from cdn');
+try
+{
+    var tab = $('a.SubTierNavSelected').text();
+
+ if(tab == 'Overview') {
+ 
+$('td[data-automation-id="Label_Status"]').text("StatusChangedFromFormScript");
+}
+}
+catch (e)
+{
+console.log(e);
+}
