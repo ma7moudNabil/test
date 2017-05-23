f7137859e507d96f7f7d34040c9be648e5dec20a
nabil <mahmoud.nabil663@gmail.com>
2017-05-23 20:57:01 +0200

    'first'

---
+++ cdn.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+    var tab = $('a.SubTierNavSelected').text();
+
+ if(tab == 'Overview') {
+ 
+$('td[data-automation-id="Label_Status"]').text("StatusChangedFromFormScript");
+}
+
+</script>
